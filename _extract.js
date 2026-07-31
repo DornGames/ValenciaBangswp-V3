b9f17562e07e2aaca8a90edb028967c3d3c31622
cgy22 <105623596+cgy22@users.noreply.github.com>
2026-07-31 16:06:59 +0800

    Delete _extract.js

--- _extract.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const data = fs.readFileSync('c:\\Users\\70495\\Desktop\\Git Repositories\\Valencia Bangswp\\Valencia Bangswp - V2\\js\\wordData.js', 'utf8');
-const dhMatches = data.matchAll(/dh:\s*['`]([^'`]+)['`]/g);
-const dhs = [];
-for (const m of dhMatches) {
-  dhs.push(m[1]);
-}
-console.log('Total entries:', dhs.length);
-console.log('All dh values:', JSON.stringify(dhs));
